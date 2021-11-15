7fafbfba06152a082942a526a8b3532eed74b2b6
wensydesousa <wensys013@gmail.com>
2021-11-14 21:43:27 -0800

    styled styles.css . changed start script back

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // IMPORT SCSS FILE TO BE SOURCE OF STYLING
-import "./styles.scss";
+import "./styles.css";
 // IMPORT ROUTER
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <Router>
